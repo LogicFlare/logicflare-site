dd8ca1de1d47f5b3914119d213f4d82a0b7e924c
JoaoPrestupa <joao_prestupa11@hotmail.com>
2024-11-01 11:11:54 -0300

    config for docker 01-11

--- dockerfile
+++ dockerfile
@@ -1,18 +1,10 @@
 FROM node:18-alpine AS base
 
 WORKDIR /app
-
 COPY package.json package-lock.json* ./
 RUN npm ci
-# ou i
 
-FROM base AS builder
-WORKDIR /app
-COPY --from=base /app/node_modules ./node_modules
 COPY . .
-
-RUN npm run build
-
 EXPOSE 3000
 
 CMD ["npm", "run", "dev"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true,  // Permite escutar em rede
+    port: 3000,  // Altera para a porta 3000
+  },
+});

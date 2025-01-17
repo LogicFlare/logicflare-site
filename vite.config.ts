cdadcdf99f8ad5977b1abfe0739e638358ff1b4a
JoaoPrestupa <joao_prestupa11@hotmail.com>
2025-01-17 15:26:51 -0300

    feat: prod v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: true,  // Permite escutar em rede
     port: 3000,  // Altera para a porta 3000
   },
-});
+})
